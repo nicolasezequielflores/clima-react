7b6d8408cd0386b8a16b39bcdff4d995cf6aa2e1
Nicolás Flores <110703674+nicolasezequielflores@users.noreply.github.com>
2025-04-23 18:48:33 -0300

    logo-ubi-ajustado

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,14 @@
 import Principal from "./Principal"
 import Weather from "./Weather"
+import './style/layout.css'
 
 const Layout = () => {
   return (
-    <>
-    <Principal />
-    <Weather />
-    </>
+    <div className="layout">
+      <Principal />
+      <Weather />
+    </div>
+
   )
 }
 
